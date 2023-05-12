eb8575895deffcbf44756a3b132a01f942156492
RyanD177 <w0477770@campus.nscc.ca>
2023-05-12 04:56:51 -0300

    intial commit gambling game.

---
+++ script.js
@@ -0,0 +1,18 @@
+// 1. How much money user has to play with
+// 2. number of lines to bet on
+// 3. Collect bet amount.
+// 4. Spin the slot machine  
+// 5. Check if the user won
+// 6. give the user their winning
+// 7. Play again
+
+function deposit () {
+
+
+
+
+    
+
+
+}
+deposit();
