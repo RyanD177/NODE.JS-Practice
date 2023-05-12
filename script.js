03ff8cca1a9285a2978dcffa7729389c1823116d
RyanD177 <w0477770@campus.nscc.ca>
2023-05-12 11:31:45 -0300

    node.js practice

--- script.js
+++ script.js
@@ -8,14 +8,55 @@
 
 const prompt = require("prompt-sync")();
 
-function deposit () {
+const deposit  = ()  => { // arrow function to get bet for the user and check if its a number or more then 0, if not then invalid is displayed.
+    while(true){
+let depositAmount = prompt("What amount do you want to deposit: ")
+let numberDepositAmount = parseFloat(depositAmount);
 
-const depositAmount = prompt("Enter a deposit amount: ")
+if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+    console.log("invalid ")
+} else{
+    return numberDepositAmount
+}   
+    }
 
-
-    
+}
+const getNumberOfLines = () => {
+    while(true){
+        let lines = prompt("Enter the number of lines to bet (1-3) : ")
+        let numberOfLines = parseFloat(lines);
+        
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >3 ){ // 3 number of lines in our bet machine.
+            console.log("invalid ")
+        } else{
+            return numberOfLines
+        }   
+            }
+        
 
 
 }
+ const getBet = (balance) => {
 
-deposit();
+    while(true){
+        let bet = prompt("Enter the total bet : ") // number of bet has to be less then your balance.
+        let numberBet = parseFloat(bet);
+        
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance ){ // if you put in a bet greater then your balance, invalid 
+            console.log("You are betting more then you have bro ")
+        } else{
+            return numberBet
+        }   
+            }
+        
+
+
+ }
+
+let balance  = deposit();
+
+
+const numberOfLines = getNumberOfLines();
+
+
+const bet = getBet(balance);
